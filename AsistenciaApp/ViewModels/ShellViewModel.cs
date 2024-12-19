01f57a7eaf9438ece97a937e2c42c738dc3f1648
heymateo <prueba22724@gmail.com>
2024-12-19 08:46:21 -0600

    Base del proyecto

---
+++ AsistenciaApp/ViewModels/ShellViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows.Input;
+
+using AsistenciaApp.Contracts.Services;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Navigation;
+
+namespace AsistenciaApp.ViewModels;
+
+public partial class ShellViewModel : ObservableRecipient
+{
+    [ObservableProperty]
+    private bool isBackEnabled;
+
+    public ICommand MenuFileExitCommand
+    {
+        get;
+    }
+
+    public ICommand MenuSettingsCommand
+    {
+        get;
+    }
+
+    public ICommand MenuViewsListDetailsCommand
+    {
+        get;
+    }
+
+    public ICommand MenuViewsContentGridCommand
+    {
+        get;
+    }
+
+    public ICommand MenuViewsDataGridCommand
+    {
+        get;
+    }
+
+    public ICommand MenuViewsMainCommand
+    {
+        get;
+    }
+
+    public INavigationService NavigationService
+    {
+        get;
+    }
+
+    public ShellViewModel(INavigationService navigationService)
+    {
+        NavigationService = navigationService;
+        NavigationService.Navigated += OnNavigated;
+
+        MenuFileExitCommand = new RelayCommand(OnMenuFileExit);
+        MenuSettingsCommand = new RelayCommand(OnMenuSettings);
+        MenuViewsListDetailsCommand = new RelayCommand(OnMenuViewsListDetails);
+        MenuViewsContentGridCommand = new RelayCommand(OnMenuViewsContentGrid);
+        MenuViewsDataGridCommand = new RelayCommand(OnMenuViewsDataGrid);
+        MenuViewsMainCommand = new RelayCommand(OnMenuViewsMain);
+    }
+
+    private void OnNavigated(object sender, NavigationEventArgs e) => IsBackEnabled = NavigationService.CanGoBack;
+
+    private void OnMenuFileExit() => Application.Current.Exit();
+
+    private void OnMenuSettings() => NavigationService.NavigateTo(typeof(SettingsViewModel).FullName!);
+
+    private void OnMenuViewsListDetails() => NavigationService.NavigateTo(typeof(ListDetailsViewModel).FullName!);
+
+    private void OnMenuViewsContentGrid() => NavigationService.NavigateTo(typeof(ContentGridViewModel).FullName!);
+
+    private void OnMenuViewsDataGrid() => NavigationService.NavigateTo(typeof(DataGridViewModel).FullName!);
+
+    private void OnMenuViewsMain() => NavigationService.NavigateTo(typeof(MainViewModel).FullName!);
+}
